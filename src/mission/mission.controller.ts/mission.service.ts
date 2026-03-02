ff19feb727e687ac487562f108cd8aa837dfbc1e
pockypycok <cheophy8@gmail.com>
2026-03-02 19:28:57 +0700

    lll;s[odkw

--- src/mission/mission.controller.ts/mission.service.ts
+++ src/mission/mission.controller.ts/mission.service.ts
@@ -1,6 +1,7 @@
 import {Injecable} from '@nestjs/common';
 import {Mission} from './mission.interface';
-
+import * as fs from 'fs';
+import * as path from 'path';
 @Injecable()
 export class MissionService {
     private missions: Mission  =[];
@@ -8,6 +9,14 @@ export class MissionService {
         return this.missions;
 
 findall():Mission [] {
-    
+    const data = fs.readFileSync(path.join(__dirname, 'missions.json')  )'utf-8');
+this.missions =JSON.parse(data);
+  Durationdays = this.missions.map(mission => {
+    if (mission.startDate && mission.endDAte) }
+    const startDate = new Date(MissionService.startDate
+        const endDate = new Date(MissionService.endDate);
+        
+    )
+
 }
 }
